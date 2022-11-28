e7ee4fc715a5a969d3115047b16bff8664049b16
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2022-11-28 09:05:10 +0100

    fixat inbounds scriptet

--- Fort-Sam/Assets/Scripts/Felicia Scripts/CameraFollow.cs
+++ Fort-Sam/Assets/Scripts/Felicia Scripts/CameraFollow.cs
@@ -4,11 +4,18 @@ public class CameraFollow : MonoBehaviour
 {
 
     public Transform Sam;
+    [SerializeField] Vector2 minPositions, maxPositions;
+    
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Sam.transform.position + new Vector3(0, 1, -5);
+        transform.position = new Vector3(Mathf.Clamp(Sam.position.x, minPositions.x, maxPositions.x), Mathf.Clamp(Sam.position.y, minPositions.y, maxPositions.y), transform.position.z); //Sam.transform.position + new Vector3(0, 1, -5);
+    }
+
+    void Awake()
+    {
+
     }
 }
 
